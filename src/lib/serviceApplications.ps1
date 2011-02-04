c4a5df767c753c3824c657b6277413547a2f9e71
Glen Cooper <glen@glenc.net>
2011-02-03 21:43:43 -0800

    Refactored service app creation code.

--- src/lib/serviceApplications.ps1
+++ src/lib/serviceApplications.ps1
@@ -2,8 +2,105 @@
 # Service Application Functions
 # ---------------------------------------------------------------
 
+
+function ProvisionMetadataServiceApplications($config) {
+    foreach ($def in $config.ServiceApplications.ManagedMetadataApplication) {
+        $serviceName = $def.name
+        $dbName = $def.DBName
+        $partitioned = $def.Partitioned -eq "True"
+        $adminAccount = GetManagedAccountUsername $def.AdminAccount $config
+        
+        info "Creating Managed Metadata Service Application"
+        debug "  Name:" $serviceName
+        debug "  AppPool:" $def.AppPool.name
+        debug "  DBName:" $dbName
+        if ($partitioned) { debug "  Partitioned" }
+        
+        $createCmd = "New-SPMetadataServiceApplication -DatabaseName `"$dbName`" -AdministratorAccount `"$adminAccount`" -FullAccessAccount `"$adminAccount`""
+        if ($partitioned) { $createCmd += " -PartitionMode" }
+        
+        $proxyCmd = "New-SPMetadataServiceApplicationProxy"
+        if ($partitioned) { $proxyCmd += " -PartitionMode" }
+        
+        ProvisionServiceApplication $def $createCmd $proxyCmd $config
+    }
+}
+
+function ProvisionUserProfileServiceApplications($config) {
+    foreach ($def in $config.ServiceApplications.UserProfileApplication) {
+        $serviceName = $def.name
+        $profileDb = $def.ProfileDB
+        $syncDb = $def.ProfileSyncDB
+        $socialDb = $def.SocialDB
+        $mySiteHostUrl = $def.MySites.HostUrl
+        $personalSitePath = $def.MySites.PersonalSitePath
+        $partitioned = $def.Partitioned -eq "True"
+        
+        info "Creating User Profile Service Application"
+        debug "  Name:" $serviceName
+        debug "  AppPool:" $def.AppPool.name
+        if ($partitioned) { debug "  Partitioned" }
+        
+        $createCmd = "New-SPProfileServiceApplication -ProfileDBName `"$profileDb`" -ProfileSyncDBName `"$syncDb`" -SocialDBName `"$socialDb`" -MySiteHostLocation `"$mySiteHostUrl`" -MySiteManagedPath `"$personalSitePath`""
+        if ($partitioned) { $createCmd += " -PartitionMode" }
+        
+        $proxyCmd = "New-SPProfileServiceApplicationProxy"
+        if ($partitioned) { $proxyCmd += " -PartitionMode" }
+        
+        CreateMySiteHost
+        
+        ProvisionServiceApplication $def $createCmd $proxyCmd $config
+        
+        # start sync service
+    }
+}
+
+function CreateMySiteHost {
+    warn "not implemented"
+    
+}
+
+function ProvisionServiceApplication($appDefinition, [string]$serviceAppCmd, [string]$proxyCmd, $config) {
+    $appName = $appDefinition.name
+    $appPoolName = $appDefinition.AppPool.name
+    try {
+        if ((Get-SPServiceApplication | ? {$_.DisplayName -eq $appName}) -eq $null) {
+            
+            # Get or create app pool
+            $appPoolAccount = GetOrCreateManagedAccount $appDefinition.AppPool.account $config
+            if ($appPoolAccount -eq $null) { throw "Managed account not found" }
+            
+            $appPool = GetOrCreateServiceApplicationPool $appPoolName $appPoolAccount
+            
+            # create actual service app
+            debug "  Creating service application..."
+            $serviceAppCmd += " -Name `"$appName`" -ApplicationPool `$appPool"
+            $createAppCmd = $ExecutionContext.InvokeCommand.NewScriptBlock($serviceAppCmd)
+            $serviceApp = Invoke-Command -ScriptBlock $createAppCmd
+            if (-not $?) { throw "- Failed to create service application" }
+            
+            # create proxy
+            debug "  Creating application proxy..."
+            $proxyCmd += " -Name `"$appName Proxy`" -ServiceApplication `$serviceApp"
+            $createProxyCmd = $ExecutionContext.InvokeCommand.NewScriptBlock($proxyCmd)
+            Invoke-Command -ScriptBlock $createProxyCmd | Out-Null
+            if (-not $?) { throw "- Failed to create service application proxy" }
+            
+            # assign permissions
+            ApplyPermissionsToServiceApplication $serviceApp $appDefinition.Permissions $config
+            
+            debug "  done"
+            
+        } else {
+            warn "Service application already exists"
+        }
+    } catch {
+        Write-Output $_
+    }
+}
+
 function GetOrCreateServiceApplicationPool([string]$name, $identity) {
-    info "Getting Application Pool $name, creating if necessary..."
+    debug "  Getting Application Pool $name, creating if necessary..."
     $appPool = Get-SPServiceApplicationPool $name -ea SilentlyContinue
     
     if ($appPool -eq $null) { 
@@ -30,146 +127,4 @@ function ApplyPermissionsToServiceApplication($serviceAppToSecure, $permissions,
     
     ## Apply the changes to the Service application
     Set-SPServiceApplicationSecurity $serviceAppIDToSecure -objectSecurity $serviceAppSecurity
-}
-
-function ProvisionMetadataServiceApp($config) {
-    foreach ($def in $config.ServiceApplications.ManagedMetadataApplication) {
-        $serviceName = $def.name
-        $partitioned = $def.Partitioned -eq "True"
-        $adminAccount = GetManagedAccountUsername $def.AdminAccount $config
-        
-        info "Creating Managed Metadata Service Application"
-        debug "  Name:" $serviceName
-        debug "  AppPool:" $def.AppPool.name
-        debug "  DBName:" $def.DBName
-        if ($partitioned) { debug "  Partitioned" }
-        
-        try {
-            ## Create a Metadata Service Application
-            if((Get-SPServiceApplication | ? {$_.DisplayName -eq $serviceName}) -eq $null) {
-                ## Get Managed Account
-                $appPoolAccount = GetOrCreateManagedAccount $def.AppPool.account $config
-                if ($appPoolAccount -eq $null) { throw "Managed Account not found" }
-                
-                # Get App Pool
-                $appPool = GetOrCreateServiceApplicationPool $def.AppPool.name $appPoolAccount
-            
-                info "Creating Managed Metadata Service"
-                
-                ## Create Service App
-                info "Creating Metadata Service Application..."
-                if ($partitioned) {
-                    $metaDataServiceApp  = New-SPMetadataServiceApplication -PartitionMode -Name $serviceName -ApplicationPool $appPool -DatabaseName $def.DBName -AdministratorAccount $adminAccount -FullAccessAccount $adminAccount
-                    if (-not $?) { throw "Failed to create Metadata Service Application" }
-                } else {
-                    $metaDataServiceApp  = New-SPMetadataServiceApplication -Name $serviceName -ApplicationPool $appPool -DatabaseName $def.DBName -AdministratorAccount $adminAccount -FullAccessAccount $adminAccount
-                    if (-not $?) { throw "Failed to create Metadata Service Application" }
-                }
-                
-
-                ## create proxy
-                info "Creating Metadata Service Application Proxy..."
-                if ($partitioned) {
-                    $metaDataServiceAppProxy  = New-SPMetadataServiceApplicationProxy -PartitionMode -Name "$serviceName Proxy" -ServiceApplication $metaDataServiceApp -DefaultProxyGroup
-                    if (-not $?) { throw "- Failed to create Metadata Service Application Proxy" }
-                } else {
-                    $metaDataServiceAppProxy  = New-SPMetadataServiceApplicationProxy -Name "$serviceName Proxy" -ServiceApplication $metaDataServiceApp -DefaultProxyGroup
-                    if (-not $?) { throw "- Failed to create Metadata Service Application Proxy" }
-                }
-                
-                
-                ## Grant Rights to App
-                info "Granting rights to Metadata Service Application..."
-                ApplyPermissionsToServiceApplication $metaDataServiceApp $def.Permissions $config
-                
-                
-                ## All Done
-                info "Done creating Managed Metadata Service."
-            } else { 
-                info "Managed Metadata Service already exists."
-            }
-        } catch {
-            Write-Output $_ 
-        }
-    }
-}
-
-function ProvisionUserProfileServiceApp($definitions) {
-    foreach ($def in $definitions) {
-        $serviceName = $def.Get_Item("Name")
-        
-        info "Creating User Profile Service Application '$serviceName'"
-        try {
-            if ((Get-SPServiceApplication -Name $serviceName) -eq $null) {
-                
-                # configure My Site Host and Site Collection
-                CreateMySiteHost
-                
-                # create app
-                $profileServiceApp = CreateUserProfileServiceApp $def
-                
-                # Create Proxy
-                CreateUserProfileServiceProxy $serviceName $profileServiceApp
-                
-                ## Grant Rights to App
-                ApplyPermissionsToServiceApplication $serviceName $def.Get_Item("Permissions")
-                
-                ## All Done
-                info "Done"
-                
-            } else {
-                warn "User profile service application was already created."
-            }
-        } catch {
-        	warn "Could not create application"
-        }
-    }
-}
-
-function CreateMySiteHost {
-    warn "not implemented"
-    
-}
-
-function CreateUserProfileServiceApp($definition) {
-    $appPoolAccountName = $def.Get_Item("AppPoolAccount")
-    $appPoolAccountPwd = $_.Get_Item("AppPoolAccountPwd")
-    $appPoolName = $_.Get_Item("AppPoolName")
-    $profileDB = $_.Get_Item("ProfileDB")
-    $syncDB = $_.Get_Item("ProfileSyncDB")
-    $socialDB = $_.Get_Item("SocialDB")
-    $partitioned = $_.Get_Item("Partitioned")
-    $mySiteUrl = $_.Get_Item("MySiteUrl")
-    $mySitePort = $_.Get_Item("MySitePort")
-
-    ## Get Managed Account
-    $appPoolAccount = GetOrCreateManagedAccount $appPoolAccountName $appPoolAccountPwd
-      if ($appPoolAccount -eq $null) { throw "Managed Account $appPoolAccountName not found" }
-    
-    # Get App Pool
-    $appPool = GetOrCreateServiceApplicationPool $appPoolName $appPoolAccount
-    
-    # Create Service App
-    if ($partitioned) {
-        New-SPProfileServiceApplication -PartitionMode -Name "$serviceName" -ApplicationPool "$appPoolName" -ProfileDBName $profileDB -ProfileSyncDBName $syncDB -SocialDBName $socialDB -MySiteHostLocation "$mySiteURL:$mySitePort"
-        if (-not $?) {throw "Failed to create user profile service"}
-    } else {
-        New-SPProfileServiceApplication -Name "$serviceName" -ApplicationPool "$appPoolName" -ProfileDBName $profileDB -ProfileSyncDBName $syncDB -SocialDBName $socialDB -MySiteHostLocation "$mySiteURL:$mySitePort"
-        if (-not $?) {throw "Failed to create user profile service"}
-    }
-    
-    debug "Wait for service to come online"
-    $profileServiceApp = Get-SPServiceApplication -Name $serviceName
-    while ($profileServiceApp.Status -ne "Online") {
-        write-progress
-        sleep 1
-        $profileServiceApp = Get-SPServiceApplication -Name $serviceName
-    }
-    
-    return $profileServiceApp
-}
-
-function CreateUserProfileServiceProxy($serviceName, $profileServiceApp) {
-    $profileServiceAppProxy  = New-SPProfileServiceApplicationProxy -Name "$serviceName Proxy" -ServiceApplication $profileServiceApp -DefaultProxyGroup
-    if (-not $?) { throw " - Failed to create $serviceName Proxy" }
 }
